83e93cb632c1b3695995417aeaa12d3479ee0486
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-07-09 13:08:07 -0400

    Create 1581_CustomerWhoVisitedButDidNotMakeAnyTransactions.sql

---
+++ BasicJoins/1581_CustomerWhoVisitedButDidNotMakeAnyTransactions.sql
@@ -0,0 +1,8 @@
+SELECT v.customer_id, COUNT(*) AS count_no_trans
+FROM Visits v
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Transactions t
+    WHERE v.visit_id = t.visit_id
+)
+GROUP BY v.customer_id;
